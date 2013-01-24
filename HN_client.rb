a568bee3d7e28491dac0ceed681f7a8572f867f6
Kriti <kritib@gmail.com>
2013-01-23 18:04:38 -0800

    Made stuff work, then realized it was broken

--- HN_client.rb
+++ HN_client.rb
@@ -9,39 +9,105 @@ require 'rest-client'
 
 
 class HomePage
+  attr_reader :posts_list
+
+  def initialize
+    @posts_list = run
+  end
+
+  def run
+
+    page = Nokogiri::HTML(File.read("hacker_news.html"))
 
 
-end
+    links = page.css("tr > td.title > a")
+    subtexts = page.css("tr > td.subtext > a")
 
-page = Nokogiri::HTML(File.read("hacker_news.html"))
+
+    # posts_list = []
+    # links.each do |link|
+    #   posts_list << [link.text, link.attr('href')]
+    # end
+
+    # subtexts_list = []
+    # subtexts.each_with_index do |subtext, i|
+    #   posts_list[i/2] << subtext.text
+    #   posts_list[i/2] << subtext.attr('href')
+    # end
+
+    # posts_list.map! do |post|
+    #   p post
+    #   Post.new(post)
+    # end
+
+    # posts_list
+    links[0]
+  end
 
 
-links = page.css("tr > td.title > a")
-subtexts = page.css("tr > td.subtext > a")
 
-links_list = []
-links.each do |link|
-  links_list << [link.text, link.attr('href')]
-end
-
-subtexts_list = []
-subtexts.each_with_index do |subtext, i|
-  links_list[i/2] << subtext.text
-  links_list[i/2] << subtext.attr('href')
 end
 
 class Post
+  attr_reader :title, :url, :user
+
+  def initialize(post_info)
+    @title = post_info[0]
+    @url = post_info[1]
+    @user = User.new(post_info[3])
+    @num_comments = post_info[4].to_i
+    @comments_id = post_info[5]
+  end
+
+  def get_comments
+    comments = []
+    comments_page = Nokogiri::HTML(RestClient.get("http://news.ycombinator.com/#{@comments_id}"))
+    comheads_info = comments_page.css("tr > td.default > div > span > a")
+    comments_info = comments_page.css("tr > td.default > span")
+
+
+    comments_info.each do |comment|
+      comments << [comment.text]
+    end
+
+    comheads_info.each_with_index do |comhead, i|
+      comments[i/2] << comhead.attr('href')
+    end
+
+    p comments[0]
+  end
+
 
 
 end
 
 class User
+  def initialize(id)
+    @screen_name = id.split('=')[1]
+    @id = id
+    @karma = nil
+  end
+
+  def get_karma
+    if @karma.nil?
+      user_page = Nokogiri::HTML(RestClient.get("http://news.ycombinator.com/#{@id}"))
+      user_info = user_page.css("form > table tr > td")
+      @karma = [user_info[5].text.to_i, user_info[7].text.to_f]
+    end
+  end
 
 end
 
 class Comment
 
 end
+
+
+
+
+
+
+
 
 
 
